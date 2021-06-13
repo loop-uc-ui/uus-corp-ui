a186b9eebb0c1cbb1a33e452313866483424df4b
loop <loopucui@gmail.com>
2021-06-13 17:15:31 -0400

    #111 fix sound

--- Source/settings/window/SettingsSoundWindow.lua
+++ Source/settings/window/SettingsSoundWindow.lua
@@ -13,8 +13,10 @@ local Buttons = {
     Footsteps = "PlayFootstepsToggleButton"
 }
 
+local adapter = SettingsSoundWindow.adapter
+
 function SettingsSoundWindow.Initialize()
-    SettingsSoundWindow.adapter:addLabel("SettingsSoundSoundSubSectionLabel", 3000390)
+    adapter:addLabel("SettingsSoundSoundSubSectionLabel", 3000390)
             :addCheckBox(Buttons.MasterVolume, true)
             :addLabel("MasterVolumeText", 1094691)
             :addLabel("MasterVolumeToggleLabel", 1077823)
@@ -29,34 +31,37 @@ function SettingsSoundWindow.Initialize()
             :addLabel("MusicVolumeVal", L""..(math.floor(UserSoundSettings.musicVolume() * 100)))
             :addCheckBox(Buttons.Footsteps, true)
             :addLabel("PlayFootstepsToggleLabel", 1078077)
+            :addSlider(SliderBars.MasterVolume, 1.0)
+            :addSlider(SliderBars.EffectsVolume, 1.0)
+            :addSlider(SliderBars.MusicVolume, 1.0)
     SettingsSoundWindow:setOffset(0):updateScrollRect()
 end
 
 function SettingsSoundWindow.UpdateVolumeSliderSettings()
-    local masterVolume = math.floor( 100 * SettingsSoundWindow.adapter.views[SliderBars.MasterVolume]:getPosition())
-    local effectsVolume = math.floor( 100 * SettingsSoundWindow.adapter.views[SliderBars.EffectsVolume]:getPosition())
-    local musicVolume = math.floor( 100 * SettingsSoundWindow.adapter.views[SliderBars.MusicVolume]:getPosition())
-    SettingsSoundWindow.adapter:addLabel("MasterVolumeVal", L""..masterVolume)
+    local masterVolume = math.floor( 100 * adapter.views[SliderBars.MasterVolume]:getPosition())
+    local effectsVolume = math.floor( 100 * adapter.views[SliderBars.EffectsVolume]:getPosition())
+    local musicVolume = math.floor( 100 * adapter.views[SliderBars.MusicVolume]:getPosition())
+    adapter:addLabel("MasterVolumeVal", L""..masterVolume)
             :addLabel("EffectsVolumeVal", L""..effectsVolume)
             :addLabel( "MusicVolumeVal", L""..musicVolume)
 end
 
 function SettingsSoundWindow.UpdateSettings()
-    SettingsSoundWindow.adapter.views[Buttons.MasterVolume]:setChecked(UserSoundSettings.masterEnabled())
-    SettingsSoundWindow.adapter.views[Buttons.EffectsVolume]:setChecked(UserSoundSettings.effectsEnabled())
-    SettingsSoundWindow.adapter.views[Buttons.MusicVolume]:setChecked(UserSoundSettings.musicEnabled())
-    SettingsSoundWindow.adapter.views[Buttons.Footsteps]:setChecked(UserSoundSettings.footstepsEnabled())
-    SettingsSoundWindow.adapter:addSlider(SliderBars.MasterVolume, UserSoundSettings.masterVolume())
-            :addSlider(SliderBars.EffectsVolume, UserSoundSettings.effectsVolume())
-            :addSlider(SliderBars.MusicVolume, UserSoundSettings.musicVolume())
+    adapter.views[Buttons.MasterVolume]:setChecked(UserSoundSettings.masterEnabled())
+    adapter.views[Buttons.EffectsVolume]:setChecked(UserSoundSettings.effectsEnabled())
+    adapter.views[Buttons.MusicVolume]:setChecked(UserSoundSettings.musicEnabled())
+    adapter.views[Buttons.Footsteps]:setChecked(UserSoundSettings.footstepsEnabled())
+    adapter.views[SliderBars.MasterVolume]:setPosition(UserSoundSettings.masterVolume())
+    adapter.views[SliderBars.EffectsVolume]:setPosition(UserSoundSettings.effectsVolume())
+    adapter.views[SliderBars.MusicVolume]:setPosition(UserSoundSettings.musicVolume())
 end
 
 function SettingsSoundWindow.OnApplyButton()
-    UserSoundSettings.masterEnabled(SettingsSoundWindow.adapter.views[Buttons.MasterVolume]:isChecked())
-    UserSoundSettings.effectsEnabled(SettingsSoundWindow.adapter.views[Buttons.EffectsVolume]:isChecked())
-    UserSoundSettings.musicEnabled(SettingsSoundWindow.adapter.views[Buttons.MusicVolume]:isChecked())
-    UserSoundSettings.footstepsEnabled(SettingsSoundWindow.adapter.views[Buttons.Footsteps]:isChecked())
-    UserSoundSettings.musicVolume(SettingsSoundWindow.adapter.views[SliderBars.MasterVolume]:getPosition())
-    UserSoundSettings.effectsVolume(SettingsSoundWindow.adapter.views[SliderBars.EffectsVolume]:getPosition())
-    UserSoundSettings.musicVolume(SettingsSoundWindow.adapter.views[SliderBars.MusicVolume]:getPosition())
+    UserSoundSettings.masterEnabled(adapter.views[Buttons.MasterVolume]:isChecked())
+    UserSoundSettings.effectsEnabled(adapter.views[Buttons.EffectsVolume]:isChecked())
+    UserSoundSettings.musicEnabled(adapter.views[Buttons.MusicVolume]:isChecked())
+    UserSoundSettings.footstepsEnabled(adapter.views[Buttons.Footsteps]:isChecked())
+    UserSoundSettings.masterVolume(adapter.views[SliderBars.MasterVolume]:getPosition())
+    UserSoundSettings.effectsVolume(adapter.views[SliderBars.EffectsVolume]:getPosition())
+    UserSoundSettings.musicVolume(adapter.views[SliderBars.MusicVolume]:getPosition())
 end
