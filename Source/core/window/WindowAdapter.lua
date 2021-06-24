01600163b1d5d87eeb3b978f1a8c69fb5e27fc46
loop <loopucui@gmail.com>
2021-06-24 10:25:07 -0400

    #134 fix typo

--- Source/core/window/WindowAdapter.lua
+++ Source/core/window/WindowAdapter.lua
@@ -107,6 +107,6 @@ end
 function WindowAdapter:addDynamicImage(id, texture, xOffset, yOffset)
     local image = DynamicImageWindow:new(id)
     image:setTexture(texture, xOffset, yOffset)
-    self.adapter[image.id] = image
+    self.views[image.id] = image
     return self
 end
