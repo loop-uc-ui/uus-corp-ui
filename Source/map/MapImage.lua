3cfba7fa0f5236b555a7d4d36f73d72e76b29483
loop <loopucui@gmail.com>
2022-02-12 13:52:54 -0500

    #173 fix file name

--- Source/map/MapImage.lua
+++ Source/map/MapImage.lua
@@ -1,17 +1,21 @@
 MapImage = BaseWindow:new()
 
-function MapImage:new(id, isCircular)
+function MapImage:new(id, isCircular, facet, area)
     local this = {
         id = id,
         isCircular = isCircular or true,
         texture = "radar_texture",
-        centerOnPlayer = true
+        centerOnPlayer = true,
+        facet = facet or RadarApi.getFacet(),
+        area = area or RadarApi.getArea(),
+        zoom = MapSettings.getZoom()
     }
     if this.isCircular then
         MapSettings.setMode(MapSettings.MODES.RADAR)
     else
         MapSettings.setMode(MapSettings.MODES.ATLAS)
     end
+    this.mazZoom = RadarApi.getMaxZoom(this.facet, this.area)
     self.__index = self
     return setmetatable(this, self)
 end
@@ -39,6 +43,6 @@ function MapImage:activate()
     local width, height = self:getDimensions()
     RadarApi.setWindowSize(width, height, true, self.centerOnPlayer)
     RadarApi.setOffset(0, 0)
-
+    self:setTexture(Radar.textureXCord(), Radar.textureYCord())
 end
 
