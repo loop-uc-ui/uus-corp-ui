9b32b417031320aa99f093c9453046363150ff8c
loop <loopucui@gmail.com>
2022-02-12 10:45:06 -0500

    #173 new data types

--- Source/core/api/WindowDataApi.lua
+++ Source/core/api/WindowDataApi.lua
@@ -4,6 +4,14 @@ function WindowDataApi.registerData(data, id)
     RegisterWindowData(data, id)
 end
 
+function WindowDataApi.registerData(data)
+    WindowDataApi.registerData(data, 0)
+end
+
 function WindowDataApi.unregisterData(data, id)
     UnregisterWindowData(data, id)
+end
+
+function WindowDataApi.unregisterData(data)
+    WindowDataApi.unregisterData(data, 0)
 end
---
+++ Source/core/data/WaypointList.lua
@@ -0,0 +1,13 @@
+WaypointList = {}
+
+function WaypointList.event()
+    return WindowData.WaypointList.Event
+end
+
+function WaypointList.type()
+    return WindowData.WaypointList.Type
+end
+
+function WaypointList.count()
+    return WindowData.WaypointList.waypointCount
+end
