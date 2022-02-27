0ec4382638fb519429a9bd99a86a5144998fefe9
loop <loopucui@gmail.com>
2022-02-26 20:21:23 -0500

    #190 gump data class

---
+++ Source/core/data/GenericGumpCore.lua
@@ -0,0 +1,90 @@
+GenericGumpCore = {}
+
+function GenericGumpCore.data()
+    return WindowData.GG_Core
+end
+
+function GenericGumpCore.stringPageIndex()
+    return WindowData.GG_Core.stringPageIndex
+end
+
+function GenericGumpCore.toolTipPageIndex()
+    return WindowData.GG_Core.toolTipPageIndex
+end
+
+function GenericGumpCore.localizedDataCount()
+    return WindowData.GG_Core.localizedDataCount
+end
+
+function GenericGumpCore.buttonIdData()
+    return WindowData.GG_Core.buttonIdData
+end
+
+function GenericGumpCore.stringDataCount()
+    return WindowData.GG_Core.stringDataCount
+end
+
+function GenericGumpCore.buttonIdDataCount()
+    return WindowData.GG_Core.buttonIdDataCount
+end
+
+function GenericGumpCore.gumpId()
+    return WindowData.GG_Core.GumpId
+end
+
+function GenericGumpCore.stringData()
+    return WindowData.GG_Core.stringData
+end
+
+function GenericGumpCore.descCount()
+    return WindowData.GG_Core.descCount
+end
+
+function GenericGumpCore.tokenPageIndex()
+    return WindowData.GG_Core.tokenPageIndex
+end
+
+function GenericGumpCore.descPageIndex()
+    return WindowData.GG_Core.descPageIndex
+end
+
+function GenericGumpCore.objectId()
+    return WindowData.GG_Core.ObjectId
+end
+
+function GenericGumpCore.desc()
+    return WindowData.GG_Core.desc
+end
+
+function GenericGumpCore.textHueDataCount()
+    return WindowData.GG_Core.textHueDataCount
+end
+
+function GenericGumpCore.textHueData()
+    return WindowData.GG_Core.textHueData
+end
+
+function GenericGumpCore.huePageIndex()
+    return WindowData.GG_Core.huePageIndex
+end
+
+function GenericGumpCore.portImgData()
+    return WindowData.GG_Core.portImgData
+end
+
+function GenericGumpCore.portPageIndex()
+    return WindowData.GG_Core.portPageIndex
+end
+
+function GenericGumpCore.tilePageIndex()
+    return WindowData.GG_Core.tilePageIndex
+end
+
+function GenericGumpCore.gumpPicNumDataCount()
+    return WindowData.GG_Core.gumppicNumDataCount
+end
+
+function GenericGumpCore.buttonPageIndex()
+    return WindowData.GG_Core.buttonPageIndex
+end
+
