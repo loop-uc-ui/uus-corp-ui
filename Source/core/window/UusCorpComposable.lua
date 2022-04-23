b903409ada11393cac8eca8d6848ba8c135b5c69
loop <loopucui@gmail.com>
2022-04-23 01:28:54 -0400

    clear warnings

--- Source/core/window/UusCorpComposable.lua
+++ Source/core/window/UusCorpComposable.lua
@@ -2,7 +2,8 @@ UusCorpComposable = {}
 UusCorpComposable.__index = UusCorpComposable
 
 function UusCorpComposable:asView(name)
-    return setmetatable({
+    self = setmetatable({
         name = name
     }, UusCorpView)
+    return self
 end
