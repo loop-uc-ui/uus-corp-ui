1072fbdf64a41641bf50a0f71e799d226280fd64
loop <loopucui@gmail.com>
2022-05-24 22:25:41 -0400

    #216 bring the window to baseline

--- Source/container/ContainerWindow.lua
+++ Source/container/ContainerWindow.lua
@@ -1,14 +1,13 @@
 ContainerWindow = {}
 ContainerWindow.Name = "ContainerWindow_"
 
-function ContainerWindow.Initalize()
+function ContainerWindow.Initialize()
     local id = Active.dynamicWindowId()
     local window = ContainerWindow.Name .. id
     WindowApi.setId(window, id)
     WindowApi.registerEventHandler(window, Container.event(), "ContainerWindow.updateContainer")
     WindowApi.registerEventHandler(window, ObjectInfo.event(), "ContainerWindow.updateObject")
     WindowDataApi.registerData(Container.type(), id)
-    Debug.Print("test")
 end
 
 function ContainerWindow.updateContainer()
@@ -17,4 +16,8 @@ end
 
 function ContainerWindow.updateObject()
 
+end
+
+function ContainerWindow.onRightClick()
+    WindowApi.destroyWindow(Active.window())
 end
