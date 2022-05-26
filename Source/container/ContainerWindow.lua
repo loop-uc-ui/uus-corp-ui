fbeb2e175ab0d131c9134430a5b27799696506cf
loop <loopucui@gmail.com>
2022-05-25 22:41:26 -0400

    #216 WIP grid

--- Source/container/ContainerWindow.lua
+++ Source/container/ContainerWindow.lua
@@ -1,5 +1,6 @@
 ContainerWindow = {}
 ContainerWindow.Name = "ContainerWindow_"
+ContainerWindow.MaxSlots = 125
 
 function ContainerWindow.Initialize()
     local id = Active.dynamicWindowId()
@@ -11,7 +12,49 @@ function ContainerWindow.Initialize()
 end
 
 function ContainerWindow.updateContainer()
-    
+    local id = Active.updateId()
+    local slots = ContainerWindow.MaxSlots
+    local window = ContainerWindow.Name .. id
+    local x, y = WindowApi.getDimensions(window)
+
+    --Public corpses may have more than the max count
+    if Container.itemCount(id) > slots then
+        slots = Container.itemCount(id)
+    end
+
+    local sizeMultiplier = 1
+
+    for i = 1, slots do
+        local slotName = window .. "Slot" .. tostring(i)
+        WindowApi.createFromTemplate(
+            slotName,
+            "GridViewSocketTemplate",
+            window
+        )
+
+        local slotX, slotY =  WindowApi.getDimensions(slotName)
+        local rowSize = sizeMultiplier * slotX
+
+        if i ~= 1 then
+            if rowSize < x then
+                WindowApi.addAnchor(
+                    slotName,
+                    "right",
+                    window .. "Slot" .. tostring(i - 1),
+                    "left"
+                )
+                sizeMultiplier = sizeMultiplier + 1
+            else
+                WindowApi.addAnchor(
+                    slotName,
+                    "bottomleft",
+                    window .. "Slot" .. tostring(i - sizeMultiplier),
+                    "topleft"
+                )
+                sizeMultiplier = 1
+            end
+        end
+    end
 end
 
 function ContainerWindow.updateObject()
--- Source/core/data/Container.lua
+++ Source/core/data/Container.lua
@@ -7,3 +7,19 @@ end
 function Container.type()
     return WindowData.ContainerWindow.Type
 end
+
+function Container.data(id)
+    return WindowData.ContainerWindow[id]
+end
+
+function Container.items(id)
+    return Container.data(id).ContainedItems
+end
+
+function Container.itemCount(id)
+    return Container.data(id).numItems
+end
+
+function Container.slots(id)
+    return Container.data(id).numCreatedSlots or 1
+end
