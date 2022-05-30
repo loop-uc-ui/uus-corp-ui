f4696d3ceeadbc39d535c014a8809bc45a454d9b
loop <loopucui@gmail.com>
2022-05-30 00:34:40 -0400

    #233 mobile arrow

---
+++ Source/core/api/AnimatedImageApi.lua
@@ -0,0 +1,17 @@
+AnimatedImageApi = {}
+
+function AnimatedImageApi.setTexture(imageName, texture)
+    AnimatedImageSetTexture(imageName, texture)
+end
+
+function AnimatedImageApi.startAnimation(imageName, startFrame, loop, hideWhenDone, delay)
+    AnimatedImageStartAnimation(imageName, startFrame, loop, hideWhenDone, delay)
+end
+
+function AnimatedImageApi.stopAnimation(imageName)
+    AnimatedImageStopAnimation(imageName)
+end
+
+function AnimatedImageApi.setPlaySpeed(imageName, fps)
+    AnimatedImageSetPlaySpeed(imageName, fps)
+end
