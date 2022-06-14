db18b917bcc97b799d1923e56c269d2b86118c3a
loop <loopucui@gmail.com>
2022-06-14 00:46:46 -0400

    #312 WIP tabs

--- Source/settings/SettingsWindow.lua
+++ Source/settings/SettingsWindow.lua
@@ -1,8 +1,27 @@
 SettingsWindow = {}
 SettingsWindow.Name = "SettingsWindow"
+SettingsWindow.Tabs = {
+    Graphics = {
+        text = "Graphics",
+        name = SettingsWindow.Name .. "GraphicsTab"
+    },
+    Sound = {
+        text = "Sound",
+        name = SettingsWindow.Name .. "SoundTab"
+    },
+    Chat = {
+        text = "Chat",
+        name = SettingsWindow.Name .. "ChatTab"
+    }
+}
 
 function SettingsWindow.onInitialize()
-
+    for _, v in pairs(SettingsWindow.Tabs) do
+        ButtonApi.setText(
+            v.name,
+            v.text
+        )
+    end
 end
 
 function SettingsWindow.onShutdown()
