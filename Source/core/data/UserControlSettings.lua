eee3a1d53cbd3e1640883284a74751b777af2d33
loop <loopucui@gmail.com>
2022-06-16 23:41:46 -0400

    #324 scroll wheel

--- Source/core/data/UserControlSettings.lua
+++ Source/core/data/UserControlSettings.lua
@@ -1,23 +1,68 @@
 UserControlSettings = {}
 
-UserControlSettings.ScrollWheelBehaviors = {
-    SystemData.Settings.GameOptions.MOUSESCROLL_ZOOM_IN_CAMERA,
-    SystemData.Settings.GameOptions.MOUSESCROLL_ZOOM_OUT_CAMERA,
-    SystemData.Settings.GameOptions.MOUSESCROLL_TARGET_NEXT_FRIENDLY,
-    SystemData.Settings.GameOptions.MOUSESCROLL_TARGET_LAST_FRIENDLY,
-    SystemData.Settings.GameOptions.MOUSESCROLL_TARGET_NEXT_ENEMY,
-    SystemData.Settings.GameOptions.MOUSESCROLL_TARGET_LAST_ENEMY,
-    SystemData.Settings.GameOptions.MOUSESCROLL_TARGET_NEXT_ANY,
-    SystemData.Settings.GameOptions.MOUSESCROLL_TARGET_LAST_ANY,
-    SystemData.Settings.GameOptions.MOUSESCROLL_CURSOR_TARGET_LAST,
-    SystemData.Settings.GameOptions.MOUSESCROLL_CURSOR_TARGET_SELF,
-    SystemData.Settings.GameOptions.MOUSESCROLL_CURSOR_TARGET_CURRENT,
-    SystemData.Settings.GameOptions.MOUSESCROLL_CYCLE_CURSOR_TARGET,
-    SystemData.Settings.GameOptions.MOUSESCROLL_CLEAR_TARGET_QUEUE,
-    SystemData.Settings.GameOptions.MOUSESCROLL_NONE
-}
-
 UserControlSettings.Keybindings = TableFormatter:sortTable(SystemData.Settings.Keybindings)
+
+function UserControlSettings.scrollWheelBehaviors()
+    return  {
+        {
+            id = SystemData.Settings.GameOptions.MOUSESCROLL_ZOOM_IN_CAMERA,
+            tid = 1079288
+        },
+        {
+            id = SystemData.Settings.GameOptions.MOUSESCROLL_ZOOM_OUT_CAMERA,
+            tid = 1079289
+
+        },
+        {
+            id = SystemData.Settings.GameOptions.MOUSESCROLL_TARGET_NEXT_FRIENDLY,
+            tid = 1079177
+        },
+        {
+            id = SystemData.Settings.GameOptions.MOUSESCROLL_TARGET_LAST_FRIENDLY,
+            tid = 1111940
+        },
+        {
+            id = SystemData.Settings.GameOptions.MOUSESCROLL_TARGET_NEXT_ENEMY,
+            tid = 1079178
+        },
+        {
+            id = SystemData.Settings.GameOptions.MOUSESCROLL_TARGET_LAST_ENEMY,
+            tid = 1111941
+        },
+        {
+            id = SystemData.Settings.GameOptions.MOUSESCROLL_TARGET_NEXT_ANY,
+            tid = 1094824
+        },
+        {
+            id = SystemData.Settings.GameOptions.MOUSESCROLL_TARGET_LAST_ANY,
+            tid = 1111942
+        },
+        {
+            id = SystemData.Settings.GameOptions.MOUSESCROLL_CURSOR_TARGET_LAST,
+            tid = 1079156
+        },
+        {
+            id = SystemData.Settings.GameOptions.MOUSESCROLL_CURSOR_TARGET_SELF,
+            tid = 1079158
+        },
+        {
+            id = SystemData.Settings.GameOptions.MOUSESCROLL_CURSOR_TARGET_CURRENT,
+            tid = 1079157
+        },
+        {
+            id = SystemData.Settings.GameOptions.MOUSESCROLL_CYCLE_CURSOR_TARGET,
+            tid = 1112413
+        },
+        {
+            id = SystemData.Settings.GameOptions.MOUSESCROLL_CLEAR_TARGET_QUEUE,
+            tid = 1115342
+        },
+        {
+            id = SystemData.Settings.GameOptions.MOUSESCROLL_NONE,
+            tid = 1011051
+        }
+    }
+end
 
 function UserControlSettings.mouseScrollUpAction(newValue)
     if newValue ~= nil then
