ee9e3b8b5830edeec86a458006cc00894922c2d5
loop <loopucui@gmail.com>
2022-06-20 20:15:04 -0400

    #347 fix health bar drag

--- Source/health_bar/HealthBarRootOverlayWindow.lua
+++ Source/health_bar/HealthBarRootOverlayWindow.lua
@@ -1,6 +1,10 @@
 HealthBarRootOverlayWindow = {}
 HealthBarRootOverlayWindow.Name = "HealthBarRootOverlayWindow"
 
+local DRAG = 3
+
+local mouseX, mouseY = 0, 0
+
 function HealthBarRootOverlayWindow.onInitialize()
     local x, y = WindowApi.getDimensions(
         WindowApi.getParent(Active.window())
@@ -30,9 +34,40 @@ function HealthBarRootOverlayWindow.onShutdown()
 end
 
 function HealthBarRootOverlayWindow.beginHealthBarDrag()
+    mouseX = MousePosition.x()
+    mouseY = MousePosition.y()
+
+    WindowApi.registerCoreEventHandler(
+        HealthBarRootOverlayWindow.Name,
+        "OnUpdate",
+        "HealthBarRootOverlayWindow.onUpdate"
+    )
+end
+
+function HealthBarRootOverlayWindow.endHealthBarDrag()
+    mouseX, mouseY = 0, 0
+
+    WindowApi.unregisterCoreEventHandler(
+        HealthBarRootOverlayWindow.Name,
+        "OnUpdate"
+    )
+end
+
+function HealthBarRootOverlayWindow.onUpdate()
+    if WindowApi.isShowing(ContextMenuWindow.Name) then
+        return
+    end
+
     local mobile = Active.mobile()
 
     if mobile == nil or mobile == 0 then
+        return
+    end
+
+    local isDragX = MousePosition.x() - mouseX >= math.abs(DRAG)
+    local isDragY = MousePosition.y() - mouseY >= math.abs(DRAG)
+
+    if not isDragX and not isDragY then
         return
     end
 
@@ -71,36 +106,4 @@ function HealthBarRootOverlayWindow.beginHealthBarDrag()
         window,
         true
     )
-end
-
-function HealthBarRootOverlayWindow.endHealthBarDrag()
-    local mobile = Active.mobile()
-
-    if mobile == nil or mobile == 0 then
-        return
-    end
-
-    local isPlayer = mobile == PlayerStatus.id()
-    local window = MobileHealthBar.Name .. mobile
-
-    if isPlayer then
-        window = PlayerHealthBar.Name
-    end
-
-    local x, y = WindowApi.getDimensions(
-        window
-    )
-
-    WindowApi.setOffsetFromParent(
-        window,
-        MousePosition.x() - x / 2,
-        MousePosition.y() - y / 2
-    )
-
-    WindowApi.setMoving(
-        window,
-        false
-    )
-
-    Active.setMobile(nil)
 end
