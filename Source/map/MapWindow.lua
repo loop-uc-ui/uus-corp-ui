93e4fcf8c4aae2d53824c82839d22993d63186b1
loop <loopucui@gmail.com>
2022-06-28 21:21:42 -0400

    #392 zoom

--- Source/map/MapWindow.lua
+++ Source/map/MapWindow.lua
@@ -2,7 +2,72 @@ MapWindow = {}
 
 MapWindow.Name = "MapWindow"
 
+MapWindow.MapImage = MapWindow.Name .. "MapImage"
+
 function MapWindow.onInitialize()
+    WindowDataApi.registerData(
+        Radar.type(),
+        0
+    )
+
+    WindowDataApi.registerData(
+        WaypointDisplay.type(),
+        0
+    )
+
+    WindowDataApi.registerData(
+        WaypointList.type(),
+        0
+    )
+
+    WindowApi.registerEventHandler(
+        MapWindow.Name,
+        Radar.event(),
+        "MapWindow.onUpdateMap"
+    )
+
+    WindowApi.registerEventHandler(
+        MapWindow.Name,
+        WaypointList.event(),
+        "MapWindow.onUpdateWaypoints"
+    )
+
+    local x, y = WindowApi.getDimensions(
+        MapWindow.MapImage
+    )
+
+    RadarApi.setWindowSize(
+        x,
+        y,
+        true,
+        true
+    )
+
+    RadarApi.setOffset(0, 0)
+
+    MapSettings.setMode(MapSettings.MODES.ATLAS)
+
+    local facet = RadarApi.getFacet()
+    local area = RadarApi.getArea()
+
+    MapWindow.onUpdateMap()
+end
+
+function MapWindow.onUpdateMap()
+    DynamicImageApi.setTextureScale(
+        MapWindow.MapImage,
+        Radar.textureScale()
+    )
+
+    DynamicImageApi.setTexture(
+        MapWindow.MapImage,
+        "radar_texture",
+        Radar.textureXCord(),
+        Radar.textureYCord()
+    )
+end
+
+function MapWindow.onUpdateWaypoints()
 
 end
 
@@ -12,4 +77,21 @@ end
 
 function MapWindow.onRightClick()
     WindowApi.destroyWindow(MapWindow.Name)
+end
+
+function MapWindow.onMouseWheel(_, _, delta)
+    local zoom = MapSettings.getZoom() - (delta * 0.2)
+    local maxZoom = RadarApi.getMaxZoom(
+        RadarApi.getFacet(),
+        RadarApi.getArea()
+    )
+
+    if zoom < -2 then
+        zoom = -2
+    elseif zoom > maxZoom then
+        zoom = maxZoom
+    end
+
+    RadarApi.setZoom(zoom)
+    MapSettings.setZoom(zoom)
 end
