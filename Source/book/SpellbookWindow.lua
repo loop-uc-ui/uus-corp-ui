ef513476807eabfc7f131ed6de7d2f613991b1ed
loop <loopucui@gmail.com>
2022-07-23 13:03:46 -0400

    #418 simplify spellbooks

--- Source/book/SpellbookWindow.lua
+++ Source/book/SpellbookWindow.lua
@@ -6,18 +6,7 @@ SpellbookWindow.Name = "Spellbook_"
 
 SpellbookWindow.Spells = {}
 
-SpellbookWindow.Lists = {
-    Magery = "MageryList",
-    Necro = "NecroList",
-    Ninja = "NinjaList",
-    Spellweaving = "SpellweavingList",
-    Chivalry = "ChivalryList",
-    Bushido = "BushidoList",
-    Mysticism = "MysticList",
-    Masteries = "MasteriesList"
-}
-
-SpellbookWindow.List = SpellbookWindow.Lists.Magery
+SpellbookWindow.List = "List"
 
 function SpellbookWindow.onInitialize()
     local id = Active.dynamicWindowId()
@@ -39,6 +28,7 @@ function SpellbookWindow.onUpdateSpells()
     local id = WindowApi.getId(Active.window())
     local data = Spells.bookData(id)
     local order = {}
+    SpellbookWindow.Spells[id] = {}
 
     for k, v in ipairs(data.spells) do
         if v == 1 then
@@ -46,7 +36,7 @@ function SpellbookWindow.onUpdateSpells()
             local _, serverId, tid, _, _, _ = AbilityApi.getAbilityData(spell)
 
             table.insert(
-                SpellbookWindow.Spells,
+                SpellbookWindow.Spells[id],
                 k,
                 {
                     id = serverId,
@@ -61,57 +51,39 @@ function SpellbookWindow.onUpdateSpells()
         end
     end
 
-    if Spells.isNecro(id) then
-        SpellbookWindow.List = SpellbookWindow.Lists.Necro
-    elseif Spells.isNinjitsu(id) then
-        SpellbookWindow.List = SpellbookWindow.Lists.Ninja
-    elseif Spells.isSpellweaving(id) then
-        SpellbookWindow.List = SpellbookWindow.Lists.Spellweaving
-    elseif Spells.isChivalry(id) then
-        SpellbookWindow.List = SpellbookWindow.Lists.Chivalry
-    elseif Spells.isBushido(id) then
-        SpellbookWindow.List = SpellbookWindow.Lists.Bushido
-    elseif Spells.isMysticism(id) then
-        SpellbookWindow.List = SpellbookWindow.Lists.Mysticism
-    elseif Spells.isMasteries(id) then
-        SpellbookWindow.List = SpellbookWindow.Lists.Masteries
-    end
+    local list = SpellbookWindow.Name .. id .. SpellbookWindow.List
 
-    for _, v in pairs(SpellbookWindow.Lists) do
-        if v ~= SpellbookWindow.List then
-            WindowApi.destroyWindow(
-                SpellbookWindow.Name .. id .. v
-            )
-        end
-    end
+    ListBoxApi.setDataTable(
+        list,
+        "SpellbookWindow.Spells.[" .. id .. "]"
+    )
 
     ListBoxApi.setDisplayOrder(
-        SpellbookWindow.Name .. id .. SpellbookWindow.List,
+        list,
         order
     )
 end
 
 function SpellbookWindow.onShutdown()
-    SpellbookWindow.Spells = {}
+    local id = WindowApi.getId(
+        Active.window()
+    )
+
+    SpellbookWindow.Spells[id] = nil
+
     WindowApi.unregisterEventHandler(
         Active.window(),
         Spells.bookDataEvent()
     )
+
     WindowDataApi.unregisterData(
         Spells.bookDataType(),
-        0
+        id
     )
 end
 
 function SpellbookWindow.onRightClick()
     WindowApi.destroyWindow(Active.window())
-end
-
-function SpellbookWindow.onPopulate(data)
-    for i = 1, #data do
-        local row = Active.window() .. SpellbookWindow.List .. "Row" .. i
-        WindowApi.setId(row, SpellbookWindow.Spells[i].id)
-    end
 end
 
 function SpellbookWindow.onDoubleClick()
