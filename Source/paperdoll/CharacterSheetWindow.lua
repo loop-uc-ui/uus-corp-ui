a4f1576598d463204d448f729981be847dccc06f
loop <loopucui@gmail.com>
2022-07-23 22:57:05 -0400

    #260 show data

--- Source/paperdoll/CharacterSheetWindow.lua
+++ Source/paperdoll/CharacterSheetWindow.lua
@@ -2,17 +2,24 @@ CharacterSheetWindow = {}
 
 CharacterSheetWindow.Name = "CharacterSheetWindow"
 
+CharacterSheetWindow.List = CharacterSheetWindow.Name .. "List"
+
+CharacterSheetWindow.Data = {}
+
 function CharacterSheetWindow.onInitialize()
     WindowDataApi.registerData(PlayerStatus.type())
+
     WindowApi.registerEventHandler(
         CharacterSheetWindow.Name,
         PlayerStatus.event(),
         "CharacterSheetWindow.onUpdate"
     )
-    Debug.Print(PlayerStatus.stats())
+
+    CharacterSheetWindow.onUpdate()
 end
 
 function CharacterSheetWindow.onShutdown()
+    CharacterSheetWindow.Data = {}
     WindowDataApi.unregisterData(PlayerStatus.type())
     WindowApi.unregisterEventHandler(
         CharacterSheetWindow.Name,
@@ -25,5 +32,39 @@ function CharacterSheetWindow.onRightClick()
 end
 
 function CharacterSheetWindow.onUpdate()
+    CharacterSheetWindow.Data = {}
+    local order = {}
+
+    for k, v in ipairs(PlayerStatus.stats()) do
+        local name = ""
+
+        if v.tid ~= nil and v.tid ~= 0 then
+            name = StringFormatter.fromTid(
+                v.tid
+            )
+        end
+
+        table.insert(
+            CharacterSheetWindow.Data,
+            k,
+            {
+                name = name
+            }
+        )
+
+        table.insert(
+            order,
+            k,
+            k
+        )
+    end
+
+    ListBoxApi.setDisplayOrder(
+        CharacterSheetWindow.List,
+        order
+    )
+end
+
+function CharacterSheetWindow.onPopulate(data)
     
 end
